64729f27eab752d85f690b4f8338ea192f777adb
jj-gaardbo <jgaard16@gmail.com>
2017-04-11 11:06:18 +0200

    Changed comment into print

--- app/src/main/java/com/example/jgaard/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jgaard/myapplication/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //Horse penis
+        System.out.println("Horse Penis");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
