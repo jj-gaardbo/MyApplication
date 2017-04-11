5c3ab774c2172aeb0d8c7d55489024ee865a1348
Andreas Hansen <Andreas Hansen>
2017-04-11 10:51:54 +0200

    Added comment

--- app/src/main/java/com/example/jgaard/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jgaard/myapplication/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            return true;//ikke godt nok
         }
 
         return super.onOptionsItemSelected(item);
